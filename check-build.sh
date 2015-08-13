559ab3d03f9fcb482bb91be4b68d87b0396b1c72
Sakhile Masoka <sakhile.harvey@gmail.com>
2015-08-13 15:16:04 +0200

    added build.sh and check-build.sh

---
+++ check-build.sh
@@ -0,0 +1,42 @@
+module load ci
+echo "About to make the modules"
+cd $WORKSPACE/$NAME-$VERSION
+ls
+echo $?
+
+make install # DESTDIR=$SOFT_DIR
+
+mkdir -p modules
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+   puts stderr "\tAdds Torque Resource Manager 2.5.13 to your environment"
+}
+
+module load gcc/4.8.2
+
+module-whatis   "$NAME $VERSION."
+setenv       TORQUE_VERSION       $VERSION
+set          TORQUE_DIR           /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+
+prepend-path 	PATH            $TORQUE_DIR/bin
+prepend-path    PATH            $TORQUE_DIR/include
+prepend-path    PATH            $TORQUE_DIR/bin
+prepend-path    MANPATH         $TORQUE_DIR/man
+prepend-path    LD_LIBRARY_PATH $TORQUE_DIR/lib
+MODULE_FILE
+) > modules/$VERSION
+mkdir -p $LIBRARIES_MODULES/$NAME
+cp modules/$VERSION $LIBRARIES_MODULES/$NAME/$VERSION
+
+# Testing module
+module avail
+module list
+module add $NAME/$VERSION
+which qsub
+
+# Add a test to check
+
