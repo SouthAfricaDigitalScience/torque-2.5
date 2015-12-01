ee619d186e3cc147f8ccd3e2c3da787f25209443
Bruce Becker <brucellino@gmail.com>
2015-12-01 16:20:01 +0100

    Added Deploy.sh and updates for style

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+# this should be run after check-build finishes.
+. /etc/profile.d/modules.sh
+echo ${SOFT_DIR}
+module add deploy
+module add gcc/${GCC_VERSION}
+echo ${SOFT_DIR}
+cd ${WORKSPACE}/${NAME}-${VERSION}
+echo "All tests have passed, will now build into ${SOFT_DIR}"
+echo "Configuring the build"
+export CC=`which gcc`
+export CXX=`which g++`
+echo "CC is ${CC} "
+echo "CXX is $CXX"
+./configure \
+--prefix=${SOFT_DIR} \
+--without-tcl \
+--with-server-home=${SOFT_DIR}/spool
+make install
+echo "Creating the modules file directory ${LIBRARIES_MODULES}"
+mkdir -p ${LIBRARIES_MODULES}/${NAME}
+(
+
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+   puts stderr "\tAdds Torque Resource Manager 2.5.13 to your environment"
+}
+
+prereq gcc/${GCC_VERSION}
+
+module-whatis   "${NAME} ${VERSION}. not for use in production, only integration. See https://github.com/SouthAfricaDigitalScience/torque-2.5"
+setenv       TORQUE_VERSION       ${VERSION}
+setenv       TORQUE_DIR           $::env(CVMFS_DIR)/$::env(SITE)/$::env(OS)/$::env(ARCH)/${NAME}/${VERSION}-gcc-${GCC_VERSION}
+
+prepend-path    PATH            $::env(TORQUE_DIR)/bin
+prepend-path    PATH            $::env(TORQUE_DIR)/include
+prepend-path    PATH            $::env(TORQUE_DIR)/bin
+prepend-path    MANPATH         $::env(TORQUE_DIR)/man
+prepend-path    LD_LIBRARY_PATH $::env(TORQUE_DIR)/lib
+) >  ${LIBRARIES_MODULES}/${NAME}/${VERSION}-gcc-${GCC_VERSION}
